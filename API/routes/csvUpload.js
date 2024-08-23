cbbdcc482d67f8b12f93bc81cfacbf3dfcb3d7a8
Dennis <dennisenwiya@gmail.com>
2024-08-22 23:10:53 -0700

    clean up

--- API/routes/csvUpload.js
+++ API/routes/csvUpload.js
@@ -1,12 +1,9 @@
-// Import necessary modules (multer, csv-parser, and your Data model)
 const express = require('express')
-// const csv = require('csv-parser');
 const csv = require('csvtojson')
-const Data = require('../models/csvModels'); // Replace with your actual data model import
+const Data = require('../models/csvModels'); 
 const Router = new express.Router()
 const multer = require('multer');
 
-// Set up Multer for file uploads (store CSV files in memory)
 const storage = multer.diskStorage({
     destination:(req,file,cb)=>{
         cb(null, "./routes/fileUploads" );
@@ -17,11 +14,9 @@ const storage = multer.diskStorage({
 });
 const upload = multer({ storage });
 
-// Define a POST route for uploading CSV files
 
 Router.post('/upload', upload.single('csvfile'),async (req, res) => {
     const jsonArray = await csv().fromFile(req.file.path)
-    // console.log(jsonArray)
     try{
         Data.insertMany(jsonArray,(err, result)=>{
             if(err){
@@ -29,7 +24,6 @@ Router.post('/upload', upload.single('csvfile'),async (req, res) => {
             }
             return res.json("added successfuly")
         })
-        // res.json(jsonArray)
     }catch(e){
         res.status(500).send(e)
     }
